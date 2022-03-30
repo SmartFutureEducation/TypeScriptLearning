f169dcf715e2575504092ec9f0ee457b14513313
Smart Future <101960708+SmartFutureEducation@users.noreply.github.com>
2022-03-30 21:28:01 +1300

    Create typescript_intro_part_4_function.ts

---
+++ typescript_intro_part_4_function.ts
@@ -0,0 +1,27 @@
+// Welcome Players one by one
+// let playerName: string
+// playerName = "Tom"
+// console.log(`Welcome ${playerName}`)
+// console.log(`Everybody shake hands with ${playerName}`)
+// console.log(`${playerName} received a welcome sticker.`)
+// console.log(`${playerName} took a sit in the room`)
+// console.log(`${playerName} feels happy.`)
+
+// playerName = "Jack"
+// console.log(`Welcome ${playerName}`)
+// console.log(`Everybody shake hands with ${playerName}`)
+// console.log(`${playerName} received a welcome sticker.`)
+// console.log(`${playerName} took a sit in the room`)
+// console.log(`${playerName} feels happy.`)
+
+// Welcome Players with function()
+function welcomeNewPlayer(name:string): void{
+    console.log(`Welcome ${name}`)
+    console.log(`Everybody shake hands with ${name}`)
+    console.log(`${name} received a welcome sticker.`)
+    console.log(`${name} took a sit in the room`)
+    console.log(`${name} feels happy.`)
+}
+
+welcomeNewPlayer('Anne')
+welcomeNewPlayer('Nigel')
